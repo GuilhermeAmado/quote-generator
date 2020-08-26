23ce5db7515c17b26172df5a0e6cf0a529506793
Guilherme <guiii@live.com>
2020-08-26 03:05:22 -0300

    Add loader & update source API

--- script.js
+++ script.js
@@ -3,29 +3,51 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterButton = document.getElementById("twitter");
 const newQuoteButton = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get quote from API
 async function getQuote() {
-    const PROXY = "https://cors-anywhere.herokuapp.com/"
-    const API = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    loading();
+    // const PROXY = "https://cors-anywhere.herokuapp.com/"
+    const API = "https://favqs.com/api/qotd";
     try {
-        const response = await fetch(PROXY + API);
+        const response = await fetch(API);
         const data = await response.json();
+        console.log(data.quote.author)
+        console.log(data.quote.body)
 
         // If Author is blank add "Unknown"
-        if (!data.quoteAuthor) {
+        if (!data.quote.author) {
             authorText.innerText = "Unknown"
         } else {
-            authorText.innerText = data.quoteAuthor;
+            authorText.innerText = data.quote.author;
         }
 
         // Reduce font size for long quotes
-        if (data.quoteText.length > 120) {
+        if (data.quote.body.length > 120) {
             quoteText.classList.add("long-quote");
         } else {
             quoteText.classList.remove("long-quote");
         }
-        quoteText.innerText = data.quoteText;
+        quoteText.innerText = data.quote.body;
+
+        // Stop loader and show quote
+        complete();
+
     } catch (error) {
         getQuote();
         console.log("Error getting quote", error);
