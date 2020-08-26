3354420b836982b99396de34adc59c6fc2bf18e3
Guilherme <guiii@live.com>
2020-08-26 01:47:12 -0300

    Add proxy to fix CORS error

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Get quote from API
 async function getQuote() {
+    const PROXY = "https://cors-anywhere.herokuapp.com/"
     const API = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try {
-        const response = await fetch(API);
+        const response = await fetch(PROXY + API);
         const data = await response.json();
         console.log(data);
     } catch (error) {
+        getQuote();
         console.log("Error getting quote", error);
     }
 }
