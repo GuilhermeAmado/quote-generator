136dc272563bc1b562d07f895c3d69fc95f87e83
Guilherme Amado <guiii@live.com>
2020-08-28 01:56:58 -0300

    Update API

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function removeLoadingSpinner() {
 async function getQuote() {
     showLoadingSpinner();
     const PROXY = "https://cors-anywhere.herokuapp.com/"
-    const API = "https://favqs.com/api/qotd";
+    const API = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     try {
         const response = await fetch(PROXY + API);
         const data = await response.json();
